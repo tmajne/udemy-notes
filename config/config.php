0f47d29d2ef61f86059f5d7708135c9040792874
ttomczyk <ttomczyk@gmail.com>
2020-05-07 20:48:04 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@
 return [
   'db' => [
       'host' => 'localhost',
-      'name' => 'udemy_1', //'udemy', //'udemy_1',
-      'user' => 'udemy', //'root', //'udemy',
-      'password' => '2D%YKvk7isb$MSE', //'root' //'2D%YKvk7isb$MSE'
+      'name' => 'xxxx',
+      'user' => 'xxxx',
+      'password' => 'xxxx'
   ]
 ];
